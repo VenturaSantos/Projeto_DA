1689791c211929bc1fbcf9304bb5270d09df6300
RedomXD <2241582@my.ipleiria.pt>
2025-05-15 09:56:42 +0100

    Primeiro commit para testing

---
+++ iTasks-ProjetoDA/Tarefa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTasks_ProjetoDA
+{
+    public class Tarefa : Utilizador
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public EstadoAtual EstadoAtual { get; set; }
+        public DateTime DataPrevistaInicio { get; set; }
+        public DateTime DataPrevistaFim { get; set; }
+        public DateTime? DataRealInicio { get; set; }
+        public DateTime? DataRealFim { get; set; }
+        public int Ordem { get; set; }
+        public int ProgramadorId { get; set; }
+        public Utilizador Programador { get; set; }
+        public int GestorId { get; set; }
+        public Utilizador Gestor { get; set; }
+        public int TipoTarefaId { get; set; }
+        public TipoTarefa TipoTarefa { get; set; }
+    }
+}
---
+++ iTasks-ProjetoDA/Utilizador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTasks_ProjetoDA
+{
+    public class Utilizador
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; } 
+        public string Tipo { get; set; }  
+    }
+}
